51c6dca434e0dd3874980cdb9ddb7634d16821c0
jackson <jlxxlyh@163.com>
2021-02-08 10:29:56 +0800

    竞争 共生关系影响

--- Q1.py
+++ Q1.py
@@ -83,6 +83,8 @@ def update_real_number(fungis, m2, threshold):
             if z != i:
                 sigma += fungis[z].competition / \
                     fungis[i].competition*fungis[z].number
+        
+        sigma = 0
 
         # 普通模式
         # fungis[i].number = fungis[i].Neq / \
@@ -96,27 +98,27 @@ def update_real_number(fungis, m2, threshold):
         #     (1+math.exp(fungis[i].a-extension_gi[i] * fungis[i].t))**2
 
         # 竞争模式
-        fungis[i].number = (fungis[i].Neq-sigma) / \
-            (1 + math.exp(fungis[i].a - (1-sigma/fungis[i].Neq)*extension_gi[i] * fungis[i].t)) #竞争模式
-
-        if fungis[i].number < 0:
-            fungis[i].number = 0
-
-        d_number = extension_gi[i] * (1-sigma/fungis[i].Neq)*fungis[i].number
-
-        #竞争模式 + 共生模式
-        # gamma = 0
-        # if fungis[i].symbiosis_index != 0:
-        #     gamma = fungis[i].symbiosis_b*fungis[int(fungis[i].symbiosis_index)].number
-        
-        # fungis[i].number = (fungis[i].Neq-sigma+gamma) / \
-        #     (1 + math.exp(fungis[i].a - (1-sigma /
-        #                                  fungis[i].Neq+gamma)*extension_gi[i] * fungis[i].t))
+        # fungis[i].number = (fungis[i].Neq-sigma) / \
+        #     (1 + math.exp(fungis[i].a - (1-sigma/fungis[i].Neq)*extension_gi[i] * fungis[i].t)) #竞争模式
 
         # if fungis[i].number < 0:
         #     fungis[i].number = 0
 
-        # d_number = extension_gi[i] * (1-sigma/fungis[i].Neq+gamma/fungis[i].Neq)*fungis[i].number
+        # d_number = extension_gi[i] * (1-sigma/fungis[i].Neq)*fungis[i].number
+
+        #竞争模式 + 共生模式
+        gamma = 0
+        if fungis[i].symbiosis_index != 0:
+            gamma = fungis[i].symbiosis_b*fungis[int(fungis[i].symbiosis_index)].number
+        
+        fungis[i].number = (fungis[i].Neq-sigma+gamma) / \
+            (1 + math.exp(fungis[i].a - (1-sigma /
+                                         fungis[i].Neq+gamma)*extension_gi[i] * fungis[i].t))
+
+        if fungis[i].number < 0:
+            fungis[i].number = 0
+
+        d_number = extension_gi[i] * (1-sigma/fungis[i].Neq+gamma/fungis[i].Neq)*fungis[i].number
 
         #竞争模式 + 共生模式 + 寄生模式 
         # gamma = 0
